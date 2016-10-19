b5a93c3a5dc4060a4c30516884d69561ad2b3605
yuriiwasaki <lilyiwasaki@gmail.com>
2016-10-20 00:05:06 +0900

    写経dot install ゲームとか

---
+++ .Trash/Camp-Challenge/CampChallenge_PHP/001.プログラミング基礎学習１/practice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<meta lang="ja">
+    <head>
+    <meta charset="utf-8">
+    <title>PHP test</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World!";
+        ?>
+    </body>
+</html>
